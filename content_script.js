6cef3aa4788adfe9cc4897c9561c0f57b309f722
Chuseok22 <bjh59629@naver.com>
2026-03-10 14:01:37 +0900

    프로젝트_기반_구조_및_Manifest_V3_설정 : feat : project init https://github.com/Chuseok22/playback-pilot/issues/1

---
+++ content_script.js
@@ -0,0 +1,6 @@
+// content_script.js
+// Issue #2: Page Context 주입 및 playbackRate override 로직 구현 예정
+// Issue #3: MutationObserver 동적 video 감지 로직 구현 예정
+// Issue #4: 플로팅 오버레이 UI 로직 구현 예정
+
+console.log('[Playback Pilot] content_script 로드됨');
---
+++ injected.js
@@ -0,0 +1,3 @@
+// injected.js
+// 페이지 컨텍스트(page context)에서 실행되는 스크립트
+// Issue #2: HTMLMediaElement.prototype.playbackRate setter override 구현 예정
